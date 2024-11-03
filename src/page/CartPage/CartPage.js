07f505c9de002afc995f1639a62ee6eabc3e6687
subeen <soobin2318@gmail.com>
2024-11-04 01:55:29 +0900

    loading spinner

--- src/page/CartPage/CartPage.js
+++ src/page/CartPage/CartPage.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import CartProductCard from "./component/CartProductCard";
 import OrderReceipt from "../PaymentPage/component/OrderReceipt";
+import LoadingSpinner from "../../common/component/LoadingSpinner"; // Import your spinner component
 import "./style/cart.style.css";
 import { getCartList } from "../../features/cart/cartSlice";
 
 const CartPage = () => {
   const dispatch = useDispatch();
-  const { cartList, totalPrice } = useSelector((state) => state.cart);
+  const { cartList, totalPrice, loading } = useSelector((state) => state.cart); // Include loading from state
 
   useEffect(() => {
-    //카트리스트 불러오기
+    // Fetch cart list
     dispatch(getCartList());
   }, [dispatch]);
 
@@ -20,7 +20,9 @@ const CartPage = () => {
     <Container>
       <Row>
         <Col xs={12} md={7}>
-          {cartList.length > 0 ? (
+          {loading ? ( 
+            <LoadingSpinner /> 
+          ) : cartList.length > 0 ? (
             cartList.map((item) => (
               <CartProductCard item={item} key={item._id} />
             ))
@@ -32,7 +34,7 @@ const CartPage = () => {
           )}
         </Col>
         <Col xs={12} md={5}>
-          <OrderReceipt cartList={cartList} totalPrice={totalPrice}/>
+          <OrderReceipt cartList={cartList} totalPrice={totalPrice} />
         </Col>
       </Row>
     </Container>
